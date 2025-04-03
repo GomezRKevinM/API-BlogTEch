454eb1c882f286b23df2c86360564677e08b7483
KevinGr <kgomezr2@unicartagena.edu.co>
2025-04-03 18:24:47 -0500

    cambiando ruta

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.get("/foro/comentarios/:id",async(req,res)=>{
 
 // autenticaciones
 
-app.post("/authLogin",async(req,res)=>{
+app.post("/login/autenticacion",async(req,res)=>{
     try{
         const datos = req.body;
         const request = await turso.execute(`SELECT * FROM usuarios WHERE usuario = :usuario AND password = :password`,{
