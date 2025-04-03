498cf43f12df6fc99609e0339fd9dbde316f6129
KevinGr <kgomezr2@unicartagena.edu.co>
2025-04-03 09:31:28 -0500

    cambios

--- index.js
+++ index.js
@@ -16,18 +16,12 @@ const app = express();
 const port = 8080;
 const host_server = "https://app-8edf8cb5-03b9-4aaa-b441-2dc3b88977d1.cleverapps.io/";
 
-app.use(cors(corsOptions));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-app.use(session({
-    secret: 'S3cUr3!s3sS10nK3y@2025',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-}));
+
 
 let query;
 
@@ -40,71 +34,13 @@ export const turso = createClient({
 app.use(express.json());
 
 
-function selecionarDatos(tabla,tipo,columna,id){
-    return new Promise(async (resolve,reject)=>{
-        switch (tipo){
-            case "Normal":
-                query="SELECT * FROM "+tabla;
-                break;
-            case "Columna":
-                query="SELECT '"+columna+"' FROM"+tabla;
-                break;
-            case "Unico":
-                query = "SELECT * FROM " + tabla + " WHERE " + columna + " = :id";
-                break;
-        }
-        const resultado = await turso.execute({
-            sql: query,
-            args: { ide:this.id }
-        })
-        resolve(resultado)
-
-    })
-}
-
-function insertarDatos(tabla,values){
-    return new Promise(async (resolve,reject)=>{
-        const keys = Object.keys(values).join(',');
-        const placeholders = Object.keys(values).map(() => '?').join(',');
-        const query = `INSERT INTO ${tabla} (${keys}) VALUES(${placeholders})`;
-        const vals = Object.values(values);
-
-        const resultado = await turso.execute(query) 
-        resolve(resultado)       
-    })
-}
-
-function actualizarDatos(tabla, valores, condicion) {
-    return new Promise(async(resolve, reject) => {
-
-        const keys = Object.keys(valores);
-        const placeholders = keys.map(key => `${key} = ?`).join(', ');
-        const query = `UPDATE ${tabla} SET ${placeholders}`;
-
-        
-        const condKeys = Object.keys(condicion);
-        const condPlaceholders = condKeys.map(key => `${key} = ?`).join(' AND ');
-        const whereClause = ` WHERE ${condPlaceholders}`;
-
-        // Combinar la consulta con la cláusula WHERE
-        const finalQuery = query + whereClause;
-
-        // Obtener los valores del objeto `valores` y las condiciones
-        const vals = [...Object.values(valores), ...Object.values(condicion)];
-
-        // Ejecutar la consulta SQL
-        
-        const resultado = await turso.execute(finalQuery, vals);
-        resolve(resultado);
-    });
-}
-
 
 // Ruta API para obtener datos
-app.get('/api/users', (req, res) => {
-    selecionarDatos("usuarios", "Normal", "*", 0)
-        .then(data => res.json(data))
-        .catch(err => res.status(500).send(err));
+app.get('/api/users', async (req, res) => {
+    const query = 'SELECT * FROM usuarios';
+    const request = await turso.query(query);
+    const data = await request.all();
+    res.status(200).json({message:"ok",data});
 });
 // app.post('/login', (req, res) => {
 //     if (!req.body || Object.keys(req.body).length === 0) {
@@ -136,12 +72,13 @@ app.get('/api/users', (req, res) => {
 //         .catch(err => res.status(500).send(err));
 // });
 
-app.get('/api/coments',(req,res)=>{
-    selecionarDatos("comentarios","Normal","*",0)
-        .then(data => res.json(data))
-        .catch(err => res.status(500).send(err))
+app.get('/api/coments',async (req,res)=>{
+    const query = 'SELECT * FROM comentarios';
+    const request = await turso.query(query);
+    const data = await request.all();
+    res.status(200).json({message:"ok",data:data,ok:true});
 })
-app.post('/api/user',(req,res)=>{
+app.post('/api/user',async (req,res)=>{
     if (!req.body || Object.keys(req.body).length === 0) {
         return res.status(400).send('El cuerpo de la solicitud está vacío o no es válido.');
     }
@@ -150,14 +87,15 @@ app.post('/api/user',(req,res)=>{
         .then(data => res.status(201).json({exito: true,data}))
         .catch(err => res.status(500).json({exito: false,error:err.message}));
 })
-app.post('/api/coment',(req,res)=>{
+app.post('/api/coment',async(req,res)=>{
     if (!req.body || Object.keys(req.body).length === 0) {
         return res.status(400).send('El cuerpo de la solicitud está vacío o no es válido.');
     }
     const values = req.body;
-    insertarDatos("comentarios", values)
-        .then(data => res.status(201).json({exito:true,data}))
-        .catch(err => res.status(500).send(err));
+    const query = 'INSERT INTO comentarios (comentario) VALUES (?)';
+    const request = await turso.query(query);
+    const data = await request.run(values.comentario);
+    res.status(200).json({message:"ok",data:data,ok:true});
 })
 app.put('/api/user/update',(req,res)=>{
     if (!req.body || Object.keys(req.body).length === 0) {
